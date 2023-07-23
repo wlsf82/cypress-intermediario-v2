bf0e5f00e012e51622892c980e4f706f0ba8d4ba
Thiago Mota <th.araujomota@gmail.com>
2023-07-23 00:51:40 -0300

    adiciona teste para funcionalidade de log out

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,9 +1,21 @@
-describe('Testa a página de login', () => {
-  it('passes', () => {
+describe('Testa as funcionalidades de login e logout', () => {
+  it('é possível logar com sucesso', () => {
     cy.login();
     cy.get('.gl-font-size-h1')
       .contains('Welcome to GitLab')
       .should('be.visible');
+  });
 
-  })
+  it.only('é possível deslogar com sucesso', () => {
+    cy.viewport(1920, 1080)
+    cy.login();
+    cy.get('[data-qa-selector="user_menu"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-qa-selector="sign_out_link"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-qa-selector="sign_in_button"]')
+      .should('be.visible');
+  });
 })
