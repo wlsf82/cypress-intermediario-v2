31c714c9046d24be64d34005d70b4a906e41cab4
Thiago Mota <th.araujomota@gmail.com>
2023-07-24 23:00:16 -0300

    adiciona cy.session para salvar a sessao e nao precisar fazer o login repetidamente

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,16 +1,13 @@
 Cypress.Commands.add('login', (
-  user = Cypress.env('user_name'),
-  password = Cypress.env('user_password'),
-) => {
-  const login = () => {
-    cy.visit('/users/sign_in')
-
-    cy.get("[data-qa-selector='login_field']").type(user)
-    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
-    cy.get("[data-qa-selector='sign_in_button']").click()
-  }
-
-  login()
+    user = Cypress.env('user_name'),
+    password = Cypress.env('user_password')
+  ) => {
+  cy.session([user, password], () => {
+    cy.visit('/users/sign_in');
+    cy.get("[data-qa-selector='login_field']").type(user);
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false });
+    cy.get("[data-qa-selector='sign_in_button']").click();
+  })
 })
 
 Cypress.Commands.add('logout', () => {
