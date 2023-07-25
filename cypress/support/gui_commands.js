b2076a7315677ace73a9ea2eb1d89b64d212a855
Thiago Mota <th.araujomota@gmail.com>
2023-07-24 23:27:11 -0300

    adiciona cacheSession para utilizar o cache em outras paginas de spec

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -7,7 +7,7 @@ Cypress.Commands.add('login', (
     cy.get("[data-qa-selector='login_field']").type(user);
     cy.get("[data-qa-selector='password_field']").type(password, { log: false });
     cy.get("[data-qa-selector='sign_in_button']").click();
-  })
+  }, { cacheAcrossSpecs: true })
 })
 
 Cypress.Commands.add('logout', () => {
