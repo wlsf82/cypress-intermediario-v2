75ed3e39aa8eacdb934531542794070b37060507
João Campos <joaocampos963@icloud.com>
2023-09-07 18:07:57 +0200

    New things

---
+++ cypress/e2e/gui/createProject.cy.js
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'; //Faker is for generating large amounts of fake (but realistic) data for testing and development.
+
+//Testing the project creation functionality
+describe('Create Project', () => {
+  beforeEach(() => {
+    cy.login(); //login on GitLab
+  });
+
+  it('successfully', () => {
+    const project = {
+      name: `project-${faker.datatype.uuid()}`, //data obtained from faker library for create Project Name
+      description: faker.random.words(5), //data obtained from faker library for create Project Description
+    };
+
+    cy.gui_createProject(project);//Actions
+
+    //Results
+    cy.url().should(
+      'be.equal',
+      `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`
+    );
+    cy.contains(project.name).should('be.visible');
+    cy.contains(project.description).should('be.visible');
+  });
+});
